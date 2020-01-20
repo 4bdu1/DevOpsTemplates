b70954eedf56d0bf9a8ded0bf06ec4e246f6606e
Abdul Aziz Tetteh <abdulaziz.tetteh@petratrust.com>
2020-01-20 13:33:19 +0000

    add helloworld ecs service script

---
+++ helloworld-ecs-service-cf-template.py
@@ -0,0 +1,84 @@
+"""Generating CloudFormation template."""
+
+from awacs.aws import Allow, Policy, Principal, Statement
+from awacs.sts import AssumeRole
+from troposphere import (ImportValue, Join, Parameter, Ref, Select, Split,
+                         Template, ecs)
+from troposphere.ecs import ContainerDefinition, TaskDefinition
+from troposphere.iam import Role
+
+t = Template()
+
+t.add_description("ECS service - Helloworld")
+
+t.add_parameter(Parameter(
+    "Tag",
+    Type="String",
+    Default="latest",
+    Description="Tag to deploy"
+))
+
+t.add_resource(TaskDefinition(
+    "task",
+    ContainerDefinitions=[
+        ContainerDefinition(
+            Image=Join("", [
+                Ref("AWS::AccountId"),
+                ".dkr.ecr.",
+                Ref("AWS::Region"),
+                ".amazonaws.com",
+                "/",
+                ImportValue("helloworld-repo"),
+                ":",
+                Ref("Tag")]),
+            Memory=32,
+            Cpu=256,
+            Name="helloworld",
+            PortMappings=[ecs.PortMapping(
+                ContainerPort=3000)]
+        )
+    ],
+))
+
+t.add_resource(Role(
+    "ServiceRole",
+    AssumeRolePolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[AssumeRole],
+                Principal=Principal("Service", ["ecs.amazonaws.com"])
+            )
+        ]
+    ),
+    Path="/",
+    ManagedPolicyArns=[
+        'arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole']
+))
+
+t.add_resource(ecs.Service(
+    "service",
+    Cluster=ImportValue(
+        Join(
+            "-",
+            [Select(0, Split("-", Ref("AWS::StackName"))),
+                "cluster-id"]
+        )
+    ),
+    DesiredCount=1,
+    TaskDefinition=Ref("task"),
+    LoadBalancers=[ecs.LoadBalancer(
+        ContainerName="helloworld",
+        ContainerPort=3000,
+        TargetGroupArn=ImportValue(
+            Join(
+                "-",
+                [Select(0, Split("-", Ref("AWS::StackName"))),
+                    "alb-helloworld-target-group"]
+            ),
+        ),
+    )],
+    Role=Ref("ServiceRole")
+))
+
+print(t.to_json())
